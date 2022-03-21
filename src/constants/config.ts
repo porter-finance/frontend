80ab215329f83276f96de82032b7dbb81fab747d
Geczy <Geczy@users.noreply.github.com>
2022-03-21 11:03:18 -0500

    add graphql (#1)

--- src/constants/config.ts
+++ src/constants/config.ts
@@ -1,6 +1,8 @@
 // Export env vars
 export const PUBLIC_URL = process.env.PUBLIC_URL
 
+export const SUBGRAPH_URL_RINKEBY = 'https://api.thegraph.com/subgraphs/name/porter-finance/porter'
+
 // API endpoints for several environments
 export const API_URL_DEVELOP_RINKEBY = process.env.REACT_APP_ADDITIONAL_SERVICES_API_URL_RINKEBY
 export const API_URL_PRODUCTION_RINKEBY =
