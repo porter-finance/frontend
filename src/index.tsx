3aa280276ce7b302cf48fb199ba684628bd03c49
Geczy <Geczy@users.noreply.github.com>
2022-03-21 11:03:18 -0500

    add graphql (#1)

--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 
+import { ApolloClient, ApolloProvider, InMemoryCache } from '@apollo/client'
 import { Web3Provider } from '@ethersproject/providers'
 import { Web3ReactProvider, createWeb3ReactRoot } from '@web3-react/core'
 import { Provider } from 'react-redux'
 
 import { NetworkContextName } from './constants'
 import './i18n'
+import { SUBGRAPH_URL_RINKEBY } from './constants/config'
 import App from './pages/App'
 import store from './state'
 import ApplicationUpdater from './state/application/updater'
@@ -16,6 +18,11 @@ import UserUpdater from './state/user/updater'
 import ThemeProvider from './theme'
 import { GlobalStyle, ThemedGlobalStyle } from './theme/globalStyle'
 import 'sanitize.css'
+
+const apolloClient = new ApolloClient({
+  uri: SUBGRAPH_URL_RINKEBY,
+  cache: new InMemoryCache(),
+})
 
 const Web3ProviderNetwork = createWeb3ReactRoot(NetworkContextName)
 
@@ -39,12 +46,14 @@ ReactDOM.render(
     <Web3ReactProvider getLibrary={getLibrary}>
       <Web3ProviderNetwork getLibrary={getLibrary}>
         <Provider store={store}>
-          <Updaters />
-          <ThemeProvider>
-            <GlobalStyle />
-            <ThemedGlobalStyle />
-            <App />
-          </ThemeProvider>
+          <ApolloProvider client={apolloClient}>
+            <Updaters />
+            <ThemeProvider>
+              <GlobalStyle />
+              <ThemedGlobalStyle />
+              <App />
+            </ThemeProvider>
+          </ApolloProvider>
         </Provider>
       </Web3ProviderNetwork>
     </Web3ReactProvider>
