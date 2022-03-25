af8ddac5025222eb0e7b5528fed7fdd7a75da7bf
luckyrobot <97241616+luckyrobot@users.noreply.github.com>
2022-03-25 12:09:09 -0700

    add portfolio page
    closes https://github.com/porter-finance/frontend-ido/issues/5

---
+++ src/hooks/useBondsPortfolio.ts
@@ -0,0 +1,35 @@
+import { gql, useQuery } from '@apollo/client'
+import { useWeb3React } from '@web3-react/core'
+
+import { getLogger } from '../utils/logger'
+import { BondInfo } from './useAllBondInfos'
+
+const logger = getLogger('useBondsPortfolio')
+
+const bondsQuery = (account: string | null | undefined) => gql`
+  query BondList {
+    bonds(first: 100, where: { owner: "${account || '0x00'}" }) {
+      id
+      name
+      symbol
+      owner
+      maturityDate
+      paymentToken
+      collateralToken
+      collateralRatio
+      convertibleRatio
+      isAuction
+    }
+  }
+`
+
+export const useBondsPortfolio = (): Maybe<BondInfo[]> => {
+  const { account } = useWeb3React()
+  const { data, error } = useQuery(bondsQuery(account))
+
+  if (error) {
+    logger.error('Error getting useBondsPortfolio info', error)
+  }
+
+  return data?.bonds
+}
