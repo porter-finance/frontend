33310d62404e8aa4a2a306505e120322cffc3a17
bookland <bookland@porter.finance>
2022-03-26 10:58:19 -0700

    remove unused code

--- src/pages/Landing/index.tsx
+++ src/pages/Landing/index.tsx
@@ -193,7 +193,6 @@ export const Landing: React.FC = () => {
   const allAuctions = useAllAuctionInfo()
   const featuredAuctions = useInterestingAuctionInfo()
   const highestVolumeAuctions = useInterestingAuctionInfo({ closedAuctions: true })
-  const price = useTokenPrice('0x7fc66500c84a76ad7e9c93437bfc5ac33e2ddae9')
 
   useSetNoDefaultNetworkId()
 
