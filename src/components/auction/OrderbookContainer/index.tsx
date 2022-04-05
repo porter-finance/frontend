bffee86aa12767ac93ebb31f30214cf5fb4ffb5f
Geczy <Geczy@users.noreply.github.com>
2022-04-05 10:29:36 -0500

    fix mobile issues

--- src/components/auction/OrderbookContainer/index.tsx
+++ src/components/auction/OrderbookContainer/index.tsx
@@ -147,7 +147,7 @@ export const OrderBookContainer = (props) => {
 
       <div className="card ">
         <div className="card-body">
-          <div className="flex justify-between mb-5">
+          <div className="flex justify-between mb-5 flex-wrap">
             <h2 className="card-title">Orderbook</h2>
 
             <div className="flex items-center">
