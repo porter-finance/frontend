e97fac12ea8414da9d9d42cb9ef0365741aa3162
Geczy <Geczy@users.noreply.github.com>
2022-04-08 14:41:38 -0500

    add no orders placeholder

--- src/components/auction/OrderbookTable/index.tsx
+++ src/components/auction/OrderbookTable/index.tsx
@@ -10,10 +10,8 @@ import { useOrderbookState } from '../../../state/orderbook/hooks'
 import { getTokenDisplay } from '../../../utils'
 import { Tooltip } from '../../common/Tooltip'
 import { calculateInterestRate } from '../../form/InterestRateInputPanel'
-import { InfoIcon } from '../../icons/InfoIcon'
 import { BaseCard } from '../../pureStyledComponents/BaseCard'
 import { Cell } from '../../pureStyledComponents/Cell'
-import { EmptyContentText, EmptyContentWrapper } from '../../pureStyledComponents/EmptyContent'
 import { Row } from '../../pureStyledComponents/Row'
 
 export interface Props {
@@ -125,11 +123,6 @@ const StyledRow = styled(Row)`
   }
 `
 
-const StyledEmptyContentWrapper = styled(EmptyContentWrapper)`
-  height: 100%;
-  min-height: 352px;
-`
-
 interface OrderBookTableProps {
   derivedAuctionInfo: DerivedAuctionInfo
   granularity: string
@@ -155,12 +148,7 @@ export const OrderBookTable: React.FC<OrderBookTableProps> = ({
 
   const noBids = !Array.isArray(bids) || bids.length === 0
 
-  return noBids || error ? (
-    <StyledEmptyContentWrapper>
-      <InfoIcon />
-      <EmptyContentText>No bids.</EmptyContentText>
-    </StyledEmptyContentWrapper>
-  ) : (
+  return (
     <OverflowWrap>
       <Table>
         <StyledRow className="pb-2" cols={'1fr 1fr 1fr 1fr 1fr 1fr'}>
@@ -201,54 +189,79 @@ export const OrderBookTable: React.FC<OrderBookTableProps> = ({
           </TableCell>
         </StyledRow>
         <TableBody>
-          {bids.map((row, i) => (
-            <StyledRow key={i}>
-              <TableCell>
-                <div className="pointer-events-none space-x-2 inline-flex items-center px-3 py-1.5 border border-transparent rounded-full shadow-sm bg-[#5BCD88] hover:none focus:outline-none focus:none">
-                  <svg
-                    fill="none"
-                    height="9"
-                    viewBox="0 0 11 9"
-                    width="11"
-                    xmlns="http://www.w3.org/2000/svg"
-                  >
-                    <path
-                      clipRule="evenodd"
-                      d="M6.03059 0.283422C5.92179 0.108161 5.7193 0 5.5 0C5.2807 0 5.07821 0.108161 4.96941 0.283422L0.0805181 8.15842C-0.0275623 8.33252 -0.0267863 8.54641 0.0825549 8.71983C0.191896 8.89326 0.393268 9 0.611111 9H10.3889C10.6067 9 10.8081 8.89326 10.9174 8.71983C11.0268 8.54641 11.0276 8.33252 10.9195 8.15842L6.03059 0.283422ZM6.08929 3C6.08929 2.68934 5.82545 2.4375 5.5 2.4375C5.17455 2.4375 4.91071 2.68934 4.91071 3V4.875C4.91071 5.18566 5.17455 5.4375 5.5 5.4375C5.82545 5.4375 6.08929 5.18566 6.08929 4.875V3ZM6.08929 6.75C6.08929 7.06066 5.82545 7.3125 5.5 7.3125C5.17455 7.3125 4.91071 7.06066 4.91071 6.75C4.91071 6.43934 5.17455 6.1875 5.5 6.1875C5.82545 6.1875 6.08929 6.43934 6.08929 6.75Z"
-                      fill="#181A1C"
-                      fillOpacity="0.5"
-                      fillRule="evenodd"
-                    />
-                  </svg>
-                  <span className="text-xs uppercase font-normal !text-[#1E1E1E]">Active</span>
-                </div>
-              </TableCell>
-              <TableCell>
-                {round(row.price, 6)} {auctioningTokenDisplay}
-              </TableCell>
-              <TableCell>
-                {calculateInterestRate(row.price, derivedAuctionInfo?.auctionEndDate)}
-              </TableCell>
-              <TableCell>
-                {round(row.volume, 6)} {auctioningTokenDisplay}
-              </TableCell>
-              <TableCell>Unknown</TableCell>
-              <TableCell>
+          {noBids && (
+            <div className="flex flex-col items-center mx-auto pt-[100px] text-[#696969] space-y-4">
+              <div>
                 <svg
                   fill="none"
-                  height="16"
-                  viewBox="0 0 16 16"
-                  width="16"
+                  height="40"
+                  viewBox="0 0 32 40"
+                  width="32"
                   xmlns="http://www.w3.org/2000/svg"
                 >
                   <path
-                    d="M4.25 1H7.49292C7.90713 1 8.24292 1.33579 8.24292 1.75C8.24292 2.1297 7.96077 2.44349 7.59469 2.49315L7.49292 2.5H4.25C3.33183 2.5 2.57881 3.20711 2.5058 4.10647L2.5 4.25V11.75C2.5 12.6682 3.20711 13.4212 4.10647 13.4942L4.25 13.5H11.75C12.6682 13.5 13.4212 12.7929 13.4942 11.8935L13.5 11.75V8C13.5 7.58579 13.8358 7.25 14.25 7.25C14.6297 7.25 14.9435 7.53215 14.9932 7.89823L15 8V11.75C15 13.483 13.6435 14.8992 11.9344 14.9949L11.75 15H4.25C2.51697 15 1.10075 13.6435 1.00514 11.9344L1 11.75V4.25C1 2.51697 2.35645 1.10075 4.06558 1.00514L4.25 1ZM14.25 1L14.2674 1.0002C14.2875 1.00066 14.3075 1.00192 14.3274 1.00398L14.25 1C14.2968 1 14.3427 1.00429 14.3872 1.01251C14.398 1.01458 14.4097 1.01701 14.4214 1.01973C14.4448 1.02517 14.4673 1.03158 14.4893 1.03898C14.4991 1.0423 14.5095 1.04603 14.5198 1.04999C14.5391 1.05745 14.5575 1.06544 14.5755 1.07414C14.5893 1.08072 14.6032 1.08798 14.617 1.09571C14.6353 1.10603 14.653 1.11705 14.6703 1.12876C14.6807 1.13569 14.6911 1.14318 14.7014 1.15097C14.7572 1.19318 14.8068 1.24276 14.8488 1.29838L14.7803 1.21967C14.8148 1.25411 14.8452 1.29107 14.8715 1.32995C14.8825 1.34634 14.8932 1.36349 14.9031 1.38108C14.9113 1.39538 14.9189 1.41001 14.9261 1.42483C14.9335 1.44027 14.9406 1.45638 14.9471 1.47277C14.9512 1.48314 14.955 1.49324 14.9585 1.5034C14.9656 1.52376 14.9721 1.54529 14.9776 1.56722C14.9814 1.5828 14.9847 1.59822 14.9876 1.6137C14.9908 1.63097 14.9935 1.64933 14.9956 1.66788C14.9975 1.68621 14.9988 1.70389 14.9995 1.72159C14.9998 1.73042 15 1.74019 15 1.75V5.25C15 5.91884 14.1908 6.25316 13.7187 5.77937L12.502 4.558L8.53033 8.53033C8.23744 8.82322 7.76256 8.82322 7.46967 8.53033C7.2034 8.26406 7.1792 7.8474 7.39705 7.55379L7.46967 7.46967L11.443 3.495L10.2314 2.27937C9.76025 1.8065 10.0952 1 10.7627 1H14.25Z"
-                    fill="#9F9F9F"
+                    d="M27.202 4.35355L27.2113 4.36285L27.2211 4.37165L31.5 8.22268V39.5H0.5V0.5H23.3484L27.202 4.35355Z"
+                    stroke="white"
+                    strokeOpacity="0.6"
                   />
+                  <path d="M7 14H25" stroke="white" strokeOpacity="0.6" />
+                  <path d="M7 19H25" stroke="white" strokeOpacity="0.6" />
+                  <path d="M7 24H25" stroke="white" strokeOpacity="0.6" />
+                  <path d="M7 29H25" stroke="white" strokeOpacity="0.6" />
                 </svg>
-              </TableCell>
-            </StyledRow>
-          ))}
+              </div>
+              <div className="text-base">No orders placed yet</div>
+            </div>
+          )}
+          {!noBids &&
+            bids.map((row, i) => (
+              <StyledRow key={i}>
+                <TableCell>
+                  <div className="pointer-events-none space-x-2 inline-flex items-center px-3 py-1.5 border border-transparent rounded-full shadow-sm bg-[#5BCD88] hover:none focus:outline-none focus:none">
+                    <svg
+                      fill="none"
+                      height="9"
+                      viewBox="0 0 11 9"
+                      width="11"
+                      xmlns="http://www.w3.org/2000/svg"
+                    >
+                      <path
+                        clipRule="evenodd"
+                        d="M6.03059 0.283422C5.92179 0.108161 5.7193 0 5.5 0C5.2807 0 5.07821 0.108161 4.96941 0.283422L0.0805181 8.15842C-0.0275623 8.33252 -0.0267863 8.54641 0.0825549 8.71983C0.191896 8.89326 0.393268 9 0.611111 9H10.3889C10.6067 9 10.8081 8.89326 10.9174 8.71983C11.0268 8.54641 11.0276 8.33252 10.9195 8.15842L6.03059 0.283422ZM6.08929 3C6.08929 2.68934 5.82545 2.4375 5.5 2.4375C5.17455 2.4375 4.91071 2.68934 4.91071 3V4.875C4.91071 5.18566 5.17455 5.4375 5.5 5.4375C5.82545 5.4375 6.08929 5.18566 6.08929 4.875V3ZM6.08929 6.75C6.08929 7.06066 5.82545 7.3125 5.5 7.3125C5.17455 7.3125 4.91071 7.06066 4.91071 6.75C4.91071 6.43934 5.17455 6.1875 5.5 6.1875C5.82545 6.1875 6.08929 6.43934 6.08929 6.75Z"
+                        fill="#181A1C"
+                        fillOpacity="0.5"
+                        fillRule="evenodd"
+                      />
+                    </svg>
+                    <span className="text-xs uppercase font-normal !text-[#1E1E1E]">Active</span>
+                  </div>
+                </TableCell>
+                <TableCell>
+                  {round(row.price, 6)} {auctioningTokenDisplay}
+                </TableCell>
+                <TableCell>
+                  {calculateInterestRate(row.price, derivedAuctionInfo?.auctionEndDate)}
+                </TableCell>
+                <TableCell>
+                  {round(row.volume, 6)} {auctioningTokenDisplay}
+                </TableCell>
+                <TableCell>Unknown</TableCell>
+                <TableCell>
+                  <svg
+                    fill="none"
+                    height="16"
+                    viewBox="0 0 16 16"
+                    width="16"
+                    xmlns="http://www.w3.org/2000/svg"
+                  >
+                    <path
+                      d="M4.25 1H7.49292C7.90713 1 8.24292 1.33579 8.24292 1.75C8.24292 2.1297 7.96077 2.44349 7.59469 2.49315L7.49292 2.5H4.25C3.33183 2.5 2.57881 3.20711 2.5058 4.10647L2.5 4.25V11.75C2.5 12.6682 3.20711 13.4212 4.10647 13.4942L4.25 13.5H11.75C12.6682 13.5 13.4212 12.7929 13.4942 11.8935L13.5 11.75V8C13.5 7.58579 13.8358 7.25 14.25 7.25C14.6297 7.25 14.9435 7.53215 14.9932 7.89823L15 8V11.75C15 13.483 13.6435 14.8992 11.9344 14.9949L11.75 15H4.25C2.51697 15 1.10075 13.6435 1.00514 11.9344L1 11.75V4.25C1 2.51697 2.35645 1.10075 4.06558 1.00514L4.25 1ZM14.25 1L14.2674 1.0002C14.2875 1.00066 14.3075 1.00192 14.3274 1.00398L14.25 1C14.2968 1 14.3427 1.00429 14.3872 1.01251C14.398 1.01458 14.4097 1.01701 14.4214 1.01973C14.4448 1.02517 14.4673 1.03158 14.4893 1.03898C14.4991 1.0423 14.5095 1.04603 14.5198 1.04999C14.5391 1.05745 14.5575 1.06544 14.5755 1.07414C14.5893 1.08072 14.6032 1.08798 14.617 1.09571C14.6353 1.10603 14.653 1.11705 14.6703 1.12876C14.6807 1.13569 14.6911 1.14318 14.7014 1.15097C14.7572 1.19318 14.8068 1.24276 14.8488 1.29838L14.7803 1.21967C14.8148 1.25411 14.8452 1.29107 14.8715 1.32995C14.8825 1.34634 14.8932 1.36349 14.9031 1.38108C14.9113 1.39538 14.9189 1.41001 14.9261 1.42483C14.9335 1.44027 14.9406 1.45638 14.9471 1.47277C14.9512 1.48314 14.955 1.49324 14.9585 1.5034C14.9656 1.52376 14.9721 1.54529 14.9776 1.56722C14.9814 1.5828 14.9847 1.59822 14.9876 1.6137C14.9908 1.63097 14.9935 1.64933 14.9956 1.66788C14.9975 1.68621 14.9988 1.70389 14.9995 1.72159C14.9998 1.73042 15 1.74019 15 1.75V5.25C15 5.91884 14.1908 6.25316 13.7187 5.77937L12.502 4.558L8.53033 8.53033C8.23744 8.82322 7.76256 8.82322 7.46967 8.53033C7.2034 8.26406 7.1792 7.8474 7.39705 7.55379L7.46967 7.46967L11.443 3.495L10.2314 2.27937C9.76025 1.8065 10.0952 1 10.7627 1H14.25Z"
+                      fill="#9F9F9F"
+                    />
+                  </svg>
+                </TableCell>
+              </StyledRow>
+            ))}
         </TableBody>
       </Table>
     </OverflowWrap>
