576da546701a4ce28ed7448b46cedea7b8251b3e
luckyrobot <97241616+luckyrobot@users.noreply.github.com>
2022-04-08 10:21:14 -0500

    make the lienar border full width

--- src/components/auction/ExtraDetailsItem/index.tsx
+++ src/components/auction/ExtraDetailsItem/index.tsx
@@ -45,7 +45,7 @@ const Link = styled(ExternalLink)`
 `
 
 const LinearBorder = () => (
-  <svg fill="none" height="2" viewBox="0 0 248 2" width="100%" xmlns="http://www.w3.org/2000/svg">
+  <svg fill="none" height="2" width="100%" xmlns="http://www.w3.org/2000/svg">
     <path d="M0 1L248 0.999978" stroke="url(#paint0_linear_8725_2152)" strokeWidth="2" />
     <defs>
       <linearGradient
