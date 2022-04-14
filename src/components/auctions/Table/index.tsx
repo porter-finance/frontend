6b723af3bae0bfb4a4a837c1e4b451e8280c21ee
luckyrobot <97241616+luckyrobot@users.noreply.github.com>
2022-04-14 09:43:41 -0500

    Portfolio page #31 (#68)
    * portfolio page
    
    * top icons
    
    * header, status pill icon, columns update
    
    * just a simple loading state for now
    
    * Product discovery page #32 (#69)
    
    * finish portfolio page
    
    2 todos
    
    missing empty table state button action clicks
    amount column in portfolio? how to get this
    
     from gql, do i have to aggregate the bids and collect their full size for each bond?
    
    * fix missing type
    
    * rename route links (#70)

--- src/components/auctions/Table/index.tsx
+++ src/components/auctions/Table/index.tsx
@@ -1,12 +1,9 @@
-import React, { useRef } from 'react'
+import React, { ReactElement, useRef } from 'react'
 import { useNavigate } from 'react-router-dom'
 import styled, { css } from 'styled-components'
 
 import { useGlobalFilter, usePagination, useTable } from 'react-table'
 
-import { ReactComponent as AuctionsLogo } from '../../../assets/svg/auctions.svg'
-import { ReactComponent as OTCLogo } from '../../../assets/svg/otc.svg'
-import { AuctionButtonOutline, LoadingBox, OTCButtonOutline } from '../../../pages/Auction'
 import { ActionButton } from '../../auction/Claimer'
 import { Dropdown } from '../../common/Dropdown'
 import { ChevronRight } from '../../icons/ChevronRight'
@@ -200,83 +197,32 @@ const PaginationItem = styled.div`
   }
 `
 
-const TBody = styled.div`
-  min-height: 266px;
-  @media (max-width: ${({ theme }) => theme.themeBreakPoints.md}) {
-    > div:first-child {
-      position: relative !important;
-    }
-
-    > div:not(:first-child) {
-      display: none !important;
-    }
-  }
-`
-
 interface Props {
-  tableData: any[]
+  data: any[]
   loading: boolean
+  title: string
+  columns: any[]
+  emptyActionClass?: string
+  emptyActionText: string
+  emptyDescription: string
+  emptyActionClick?: () => void
+  emptyLogo: ReactElement
+  legendIcons: ReactElement
 }
 
-const columns = [
-  {
-    Header: 'Issuer',
-    accessor: 'issuer',
-    align: 'flex-start',
-    show: true,
-    style: { height: '100%', justifyContent: 'center' },
-    filter: 'searchInTags',
-  },
-  {
-    Header: 'Offering',
-    accessor: 'offering',
-    align: 'flex-start',
-    show: true,
-    style: {},
-    filter: 'searchInTags',
-  },
-  {
-    Header: 'Size',
-    accessor: 'size',
-    align: 'flex-start',
-    show: true,
-    style: {},
-    filter: 'searchInTags',
-  },
-  {
-    Header: 'Interest Rate',
-    accessor: 'interestRate',
-    align: 'flex-start',
-    show: true,
-    style: {},
-    filter: 'searchInTags',
-  },
-  {
-    Header: 'Price',
-    accessor: 'price',
-    align: 'flex-start',
-    show: true,
-    style: {},
-    filter: 'searchInTags',
-  },
-  {
-    Header: 'Status',
-    accessor: 'status',
-    align: 'flex-start',
-    show: true,
-    style: {},
-    filter: 'searchInTags',
-  },
-  {
-    Header: '',
-    accessor: 'url',
-    align: '',
-    show: false,
-    style: {},
-  },
-]
-
-const AllAuctions = ({ loading, tableData, ...restProps }: Props) => {
+const Table = ({
+  columns,
+  data,
+  emptyActionClass,
+  emptyActionClick,
+  emptyActionText,
+  emptyDescription,
+  emptyLogo,
+  legendIcons,
+  loading,
+  title,
+  ...restProps
+}: Props) => {
   const navigate = useNavigate()
 
   const globalFilter = React.useMemo(
@@ -302,7 +248,7 @@ const AllAuctions = ({ loading, tableData, ...restProps }: Props) => {
   } = useTable(
     {
       columns,
-      data: tableData,
+      data,
       globalFilter,
       initialState: { pageIndex: 0, pageSize: 10 },
     },
@@ -327,22 +273,9 @@ const AllAuctions = ({ loading, tableData, ...restProps }: Props) => {
     <Wrapper ref={sectionHead} {...restProps}>
       <div className="py-2 flex content-center justify-center md:justify-between flex-wrap items-end mb-10">
         <div className="flex flex-col space-y-4">
-          <SectionTitle>Offerings</SectionTitle>
+          <SectionTitle>{title}</SectionTitle>
 
-          <div className="flex flex-row space-x-4 items-center">
-            <div className="rounded-full bg-black px-4 py-2 text-white text-xs uppercase">All</div>
-            <svg
-              fill="none"
-              height="16"
-              viewBox="0 0 2 16"
-              width="2"
-              xmlns="http://www.w3.org/2000/svg"
-            >
-              <path d="M1 0V16" opacity="0.5" stroke="white" />
-            </svg>
-            <AuctionButtonOutline plural />
-            <OTCButtonOutline />
-          </div>
+          <div className="flex flex-row space-x-4 items-center">{legendIcons}</div>
         </div>
 
         <div>
@@ -369,51 +302,61 @@ const AllAuctions = ({ loading, tableData, ...restProps }: Props) => {
         </div>
       </div>
 
-      {loading && <LoadingBox height={600} />}
-
-      {!loading && (
-        <div className="min-h-[385px]">
-          <table className="table w-full h-full" {...getTableProps()}>
-            <thead>
-              {headerGroups.map((headerGroup, i) => (
-                <tr
-                  className="border-b border-b-[#D5D5D519]"
-                  key={i}
-                  {...headerGroup.getHeaderGroupProps()}
-                >
-                  {headerGroup.headers.map(
-                    (column, i) =>
-                      column.render('show') && (
-                        <th
-                          className="bg-transparent text-[#696969] text-[10px] font-normal"
-                          key={i}
-                          {...column.getHeaderProps()}
-                        >
-                          {column.render('Header')}
-                        </th>
-                      ),
-                  )}
+      <div className="min-h-[385px]">
+        <table className="table w-full h-full" {...getTableProps()}>
+          <thead>
+            {headerGroups.map((headerGroup, i) => (
+              <tr
+                className="border-b border-b-[#D5D5D519]"
+                key={i}
+                {...headerGroup.getHeaderGroupProps()}
+              >
+                {headerGroup.headers.map(
+                  (column, i) =>
+                    column.render('show') && (
+                      <th
+                        className="bg-transparent text-[#696969] text-[10px] font-normal"
+                        key={i}
+                        {...column.getHeaderProps()}
+                      >
+                        {column.render('Header')}
+                      </th>
+                    ),
+                )}
+              </tr>
+            ))}
+          </thead>
+          <tbody {...getTableBodyProps()}>
+            {loading &&
+              [...Array(10).keys()].map((z) => (
+                <tr className="bg-transparent text-[#D2D2D2] text-sm" key={z}>
+                  {[...Array(columns.length - 1).keys()].map((i) => (
+                    <td className="bg-transparent text-center text-[#696969]" key={i}>
+                      <div className="my-4 max-w-sm w-full animate-pulse h-4 bg-gradient-to-r to-[#181A1C] from-[#1F2123] rounded"></div>
+                    </td>
+                  ))}
                 </tr>
               ))}
-            </thead>
-            <tbody {...getTableBodyProps()}>
-              {!page.length && (
-                <tr className="bg-transparent text-[#D2D2D2] text-sm">
-                  <td
-                    className="bg-transparent text-center py-[100px] text-[#696969] space-y-7"
-                    colSpan={6}
+
+            {!loading && !page.length && (
+              <tr className="bg-transparent text-[#D2D2D2] text-sm">
+                <td
+                  className="bg-transparent text-center py-[100px] text-[#696969] space-y-7"
+                  colSpan={columns.length - 1}
+                >
+                  <div className="flex justify-center space-x-4 opacity-60">{emptyLogo}</div>
+                  <div className="text-base text-[#696969]">{emptyDescription}</div>
+                  <ActionButton
+                    className={`!w-[236px] !h-[41px] ${emptyActionClass}`}
+                    onClick={emptyActionClick}
                   >
-                    <div className="flex justify-center space-x-4 opacity-60">
-                      <AuctionsLogo height={36} width={36} /> <OTCLogo height={36} width={36} />
-                    </div>
-                    <div className="text-base text-[#696969]">
-                      There are no offerings at the moment
-                    </div>
-                    <ActionButton className="w-[236px] h-[41px]">Get notify</ActionButton>
-                  </td>
-                </tr>
-              )}
-              {page.map((row, i) => {
+                    {emptyActionText}
+                  </ActionButton>
+                </td>
+              </tr>
+            )}
+            {!loading &&
+              page.map((row, i) => {
                 prepareRow(row)
                 return (
                   <tr
@@ -434,49 +377,46 @@ const AllAuctions = ({ loading, tableData, ...restProps }: Props) => {
                   </tr>
                 )
               })}
-            </tbody>
-          </table>
+          </tbody>
+        </table>
 
-          <Pagination>
-            <PaginationBlock>
-              <PaginationText>Items per page</PaginationText>{' '}
-              <DropdownPagination
-                dropdownButtonContent={
-                  <PaginationDropdownButton>{pageSize} ▼</PaginationDropdownButton>
-                }
-                items={[5, 10, 20, 30].map((pageSize) => (
-                  <PaginationItem
-                    key={pageSize}
-                    onClick={() => {
-                      setPageSize(Number(pageSize))
-                    }}
-                  >
-                    {pageSize}
-                  </PaginationItem>
-                ))}
-              />
-            </PaginationBlock>
-            <PaginationBreak>|</PaginationBreak>
-            <PaginationBlock>
-              <PaginationText>
-                {pageIndex + 1 === 1 ? 1 : pageIndex * pageSize + 1} -{' '}
-                {rows.length < (pageIndex + 1) * pageSize
-                  ? rows.length
-                  : (pageIndex + 1) * pageSize}{' '}
-                of {rows.length} auctions
-              </PaginationText>{' '}
-              <PaginationButton disabled={!canPreviousPage} onClick={() => handlePrevPage()}>
-                <ChevronLeft />
-              </PaginationButton>
-              <PaginationButton disabled={!canNextPage} onClick={() => handleNextPage()}>
-                <ChevronRight />
-              </PaginationButton>
-            </PaginationBlock>
-          </Pagination>
-        </div>
-      )}
+        <Pagination>
+          <PaginationBlock>
+            <PaginationText>Items per page</PaginationText>{' '}
+            <DropdownPagination
+              dropdownButtonContent={
+                <PaginationDropdownButton>{pageSize} ▼</PaginationDropdownButton>
+              }
+              items={[5, 10, 20, 30].map((pageSize) => (
+                <PaginationItem
+                  key={pageSize}
+                  onClick={() => {
+                    setPageSize(Number(pageSize))
+                  }}
+                >
+                  {pageSize}
+                </PaginationItem>
+              ))}
+            />
+          </PaginationBlock>
+          <PaginationBreak>|</PaginationBreak>
+          <PaginationBlock>
+            <PaginationText>
+              {pageIndex + 1 === 1 ? 1 : pageIndex * pageSize + 1} -{' '}
+              {rows.length < (pageIndex + 1) * pageSize ? rows.length : (pageIndex + 1) * pageSize}{' '}
+              of {rows.length} auctions
+            </PaginationText>{' '}
+            <PaginationButton disabled={!canPreviousPage} onClick={() => handlePrevPage()}>
+              <ChevronLeft />
+            </PaginationButton>
+            <PaginationButton disabled={!canNextPage} onClick={() => handleNextPage()}>
+              <ChevronRight />
+            </PaginationButton>
+          </PaginationBlock>
+        </Pagination>
+      </div>
     </Wrapper>
   )
 }
 
-export default AllAuctions
+export default Table
