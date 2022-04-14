b34fb30b3c06c8ef2bc31ffc89f2f34bc35aeeee
luckyrobot <97241616+luckyrobot@users.noreply.github.com>
2022-04-14 09:42:43 -0500

    Product discovery page #32 (#69)
    * finish portfolio page
    
    2 todos
    
    missing empty table state button action clicks
    amount column in portfolio? how to get this
    
     from gql, do i have to aggregate the bids and collect their full size for each bond?
    
    * fix missing type
    
    * rename route links (#70)

--- src/components/navigation/Routes/Routes.tsx
+++ src/components/navigation/Routes/Routes.tsx
@@ -1,30 +1,26 @@
 import React from 'react'
-import { Route, Routes } from 'react-router-dom'
+import { Navigate, Route, Routes } from 'react-router-dom'
 
 import Auction from '../../../pages/Auction'
 import BondDetail from '../../../pages/BondDetail'
-import BondsOverview from '../../../pages/Bonds'
 import CreateBond from '../../../pages/CreateBond'
-import { Landing } from '../../../pages/Landing'
-import { Licenses } from '../../../pages/Licenses'
 import Offerings from '../../../pages/Offerings'
 import Portfolio from '../../../pages/Portfolio'
-import { Terms } from '../../../pages/Terms'
+import Products from '../../../pages/Products'
 import { BaseCard } from '../../pureStyledComponents/BaseCard'
 
 const AppRoutes: React.FC = () => {
   return (
     <Routes>
-      <Route element={<Auction />} path="/auctions/:auctionId/:chainId" />
-      <Route element={<Offerings />} path="/auctions" />
-      <Route element={<BondsOverview />} path="/bonds" />
-      <Route element={<CreateBond />} path="/bonds/create" />
-      <Route element={<BondDetail />} path="/bonds/:bondId" />
+      <Route element={<Auction />} path="/offerings/:auctionId/:chainId" />
+      <Route element={<Offerings />} path="/offerings" />
+      <Route element={<Products />} path="/products" />
+      <Route element={<CreateBond />} path="/offerings/create" />
+      <Route element={<BondDetail />} path="/products/:bondId" />
       <Route element={<Portfolio />} path="/portfolio" />
-      <Route element={<Landing />} path="/start" />
-      <Route element={<Terms />} path="/terms-and-conditions" />
-      <Route element={<Licenses />} path="/licenses" />
-      <Route element={<Landing />} path="/" />
+      <Route element={<Navigate replace to="/offerings" />} path="/start" />
+      <Route element={<Navigate replace to="/offerings" />} path="/auctions" />
+      <Route element={<Navigate replace to="/offerings" />} path="/" />
       <Route element={<BaseCard>Page not found Error 404</BaseCard>} path="*" />
     </Routes>
   )
