f4c3d62608e0dfb15eccef3f2225cf1e5ea9d153
Geczy <Geczy@users.noreply.github.com>
2022-04-21 12:40:48 -0500

    use error not warning

--- src/components/auction/AuctionBody/index.tsx
+++ src/components/auction/AuctionBody/index.tsx
@@ -21,7 +21,7 @@ interface AuctionBodyProps {
   derivedAuctionInfo: DerivedAuctionInfo
 }
 
-const DisabledCountryWarning = () => (
+const DisabledCountryError = () => (
   <div className="card card-bordered">
     <div className="card-body">
       <div className="flex flex-row items-center space-x-2">
@@ -35,11 +35,11 @@ const DisabledCountryWarning = () => (
           <path
             clipRule="evenodd"
             d="M7.801 14C11.7702 14 14.9879 10.866 14.9879 7C14.9879 3.13401 11.7702 0 7.801 0C3.83179 0 0.614105 3.13401 0.614105 7C0.614105 10.866 3.83179 14 7.801 14ZM6.80125 3.52497C6.78659 3.23938 7.02037 3 7.31396 3H8.28804C8.58162 3 8.81541 3.23938 8.80075 3.52497L8.59541 7.52497C8.58175 7.79107 8.35625 8 8.0827 8H7.5193C7.24575 8 7.02025 7.79107 7.00659 7.52497L6.80125 3.52497ZM6.7743 10C6.7743 9.44772 7.23397 9 7.801 9C8.36803 9 8.8277 9.44772 8.8277 10C8.8277 10.5523 8.36803 11 7.801 11C7.23397 11 6.7743 10.5523 6.7743 10Z"
-            fill="#EDA651"
+            fill="#D25453"
             fillRule="evenodd"
           />
         </svg>
-        <span className="text-[#EDA651]">Warning</span>
+        <span className="text-[#D25453]">Error</span>
       </div>
       <div className="text-sm text-[#9F9F9F]">
         Bonds are not available to people or companies who are residents of, or are located,
@@ -48,7 +48,7 @@ const DisabledCountryWarning = () => (
       </div>
       <div className="card-actions mt-6">
         <a
-          className="btn btn-sm btn-warning normal-case text-sm font-normal"
+          className="btn btn-sm btn-error normal-case text-sm font-normal"
           href="https://docs.porter.finance/"
           rel="noreferrer"
           target="_blank"
@@ -147,7 +147,7 @@ const AuctionBody = (props: AuctionBodyProps) => {
                   derivedAuctionInfo={derivedAuctionInfo}
                 />
               )}
-              {disabledCountry && <DisabledCountryWarning />}
+              {disabledCountry && <DisabledCountryError />}
             </>
           }
         />
