2395b30fd4e326294d3ea303cf4e6e614610b4d8
bookland <bookland@porter.finance>
2022-04-28 15:07:41 -0500

    use native loading

--- src/pages/Offerings/index.tsx
+++ src/pages/Offerings/index.tsx
@@ -74,7 +74,7 @@ const columns = [
 
 const Offerings = () => {
   const { chainId } = useActiveWeb3React()
-  const { data: allAuctions } = useAuctions()
+  const { data: allAuctions, loading } = useAuctions()
   const tableData = []
 
   useSetNoDefaultNetworkId()
@@ -125,11 +125,6 @@ const Offerings = () => {
     })
   })
 
-  const isLoading = React.useMemo(
-    () => allAuctions === undefined || allAuctions === null,
-    [allAuctions],
-  )
-
   return (
     <Table
       columns={columns}
@@ -149,7 +144,7 @@ const Offerings = () => {
           <OTCButtonOutline />
         </>
       }
-      loading={isLoading}
+      loading={loading}
       name={'offerings'}
       title="Offerings"
     />
