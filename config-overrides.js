3f8cd2820877530ce72a047bdff6da479f21d500
= <kyletrusler@gmail.com>
2022-05-03 07:57:48 -0400

    add cors config overrides

--- config-overrides.js
+++ config-overrides.js
@@ -1,17 +1,32 @@
 const webpack = require('webpack')
 
-module.exports = function override(config) {
-  const fallback = config.resolve.fallback || {}
-  Object.assign(fallback, {
-    crypto: require.resolve('crypto-browserify'),
-    stream: require.resolve('stream-browserify'),
-    assert: require.resolve('assert'),
-    http: require.resolve('stream-http'),
-    https: require.resolve('https-browserify'),
-    os: require.resolve('os-browserify'),
-    url: require.resolve('url'),
-  })
-  config.resolve.fallback = fallback
-  config.ignoreWarnings = [/Failed to parse source map/]
-  return config
+module.exports = {
+  webpack: function (config) {
+    const fallback = config.resolve.fallback || {}
+    Object.assign(fallback, {
+      crypto: require.resolve('crypto-browserify'),
+      stream: require.resolve('stream-browserify'),
+      assert: require.resolve('assert'),
+      http: require.resolve('stream-http'),
+      https: require.resolve('https-browserify'),
+      os: require.resolve('os-browserify'),
+      url: require.resolve('url'),
+    })
+    config.resolve.fallback = fallback
+    config.ignoreWarnings = [/Failed to parse source map/]
+    return config
+  },
+  devServer: function (config) {
+    return function (proxy, allowedHost) {
+      const modifiedConfig = config(proxy, allowedHost)
+
+      modifiedConfig.headers = {
+        'Access-Control-Allow-Origin': '*',
+        'Access-Control-Allow-Methods': 'GET',
+        'Access-Control-Allow-Headers': 'X-Requested-With, content-type, Authorization',
+      }
+
+      return modifiedConfig
+    }
+  },
 }
