3f69f4bc533ae92f7e36acb41547dda8d8fe2c85
Geczy <Geczy@users.noreply.github.com>
2022-05-31 10:59:20 -0500

    dont check vercel env

--- src/connectors/index.ts
+++ src/connectors/index.ts
@@ -22,7 +22,7 @@ chainIds.forEach((chainId: ChainId) => {
   }
 })
 
-export const isDev = process.env.REACT_APP_VERCEL_ENV !== 'production'
+export const isDev = !window.location.href.includes('app.porter')
 const defaultChainId = isDev ? 4 : 1
 
 export const network = new NetworkConnector({ urls, defaultChainId })
