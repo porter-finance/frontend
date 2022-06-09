d201dbe9f08688b966d176ffeb3b33c94b76c0ed
Geczy <Geczy@users.noreply.github.com>
2022-06-09 09:34:10 -0500

    rm unuseds

--- src/connectors/index.ts
+++ src/connectors/index.ts
@@ -1,14 +1,7 @@
-import { WalletConnectConnector } from '@anxolin/walletconnect-connector'
-import { InjectedConnector } from '@web3-react/injected-connector'
 import { NetworkConnector } from '@web3-react/network-connector'
-import { PortisConnector } from '@web3-react/portis-connector'
-import { WalletLinkConnector } from '@web3-react/walletlink-connector'
 import { chain } from 'wagmi'
 
-import { NETWORK_URL_MAINNET, PORTIS_ID } from '../constants/config'
 import { ChainId, NETWORK_CONFIGS } from './../utils/index'
-
-const POLLING_INTERVAL = 10000
 
 const urls: string[] = []
 
@@ -27,45 +20,3 @@ export const isRinkeby = !window.location.href.includes('app.porter')
 export const requiredChain = isRinkeby ? chain.rinkeby : chain.mainnet
 
 export const network = new NetworkConnector({ urls, defaultChainId: requiredChain.id })
-
-export const injected = new InjectedConnector({
-  supportedChainIds: chainIds,
-})
-
-export const walletconnect = {
-  1: new WalletConnectConnector({
-    rpc: { 1: NETWORK_CONFIGS[1].rpc },
-    bridge: 'https://safe-walletconnect.gnosis.io',
-    qrcode: true,
-    pollingInterval: POLLING_INTERVAL,
-  }),
-  4: new WalletConnectConnector({
-    rpc: { 4: NETWORK_CONFIGS[4].rpc },
-    bridge: 'https://safe-walletconnect.gnosis.io',
-    qrcode: true,
-    pollingInterval: POLLING_INTERVAL,
-  }),
-  // if no network is defined, we look whether wallet connect supports all possible chains
-  // this might cause issues on the auction overview page.
-  // A solution for this usecase should be a network selector
-  undefined: new WalletConnectConnector({
-    rpc: rpcs,
-    bridge: 'https://safe-walletconnect.gnosis.io',
-    qrcode: true,
-    pollingInterval: POLLING_INTERVAL,
-  }),
-}
-
-// mainnet only
-export const portis = new PortisConnector({
-  dAppId: PORTIS_ID,
-  networks: [1],
-})
-
-// mainnet only
-export const walletlink = new WalletLinkConnector({
-  url: NETWORK_URL_MAINNET,
-  appName: 'PorterFinance',
-  appLogoUrl:
-    'https://mpng.pngfly.com/20181202/bex/kisspng-emoji-domain-unicorn-pin-badges-sticker-unicorn-tumblr-emoji-unicorn-iphoneemoji-5c046729264a77.5671679315437924251569.jpg',
-})
