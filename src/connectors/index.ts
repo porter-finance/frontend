e83abba381491aba75fc91ba6779c8c0a73ee7dc
Geczy <Geczy@users.noreply.github.com>
2022-06-10 10:29:02 -0500

    unuseds

--- src/connectors/index.ts
+++ src/connectors/index.ts
@@ -1,22 +1,4 @@
-import { NetworkConnector } from '@web3-react/network-connector'
 import { chain } from 'wagmi'
-
-import { ChainId, NETWORK_CONFIGS } from './../utils/index'
-
-const urls: string[] = []
-
-// TOOD Try to use reduce to improve types
-const rpcs: any = {}
-
-const chainIds = Object.keys(NETWORK_CONFIGS).map(Number)
-chainIds.forEach((chainId: ChainId) => {
-  if (NETWORK_CONFIGS[chainId].rpc) {
-    urls[chainId] = NETWORK_CONFIGS[chainId].rpc
-    rpcs[chainId] = NETWORK_CONFIGS[chainId].rpc
-  }
-})
 
 export const isRinkeby = !window.location.href.includes('app.porter')
 export const requiredChain = isRinkeby ? chain.hardhat : chain.mainnet
-
-export const network = new NetworkConnector({ urls, defaultChainId: requiredChain.id })
