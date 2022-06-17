b1fcc99cee21413fa287009a72438f640d2869bd
Kyle <kyletrusler@gmail.com>
2022-06-17 14:00:54 +0200

    add no bond state

--- src/components/ProductCreate/CollateralTokenSelector.tsx
+++ src/components/ProductCreate/CollateralTokenSelector.tsx
@@ -1,7 +1,9 @@
 import React, { useMemo } from 'react'
 
+import { useAccount } from 'wagmi'
+
 import { Selector } from './BorrowTokenSelector'
-import { BorrowTokens } from './SelectableTokens'
+import { BorrowTokens, PRTRIcon } from './SelectableTokens'
 import { BondTokenDetails, TokenDetails } from './SetupProduct'
 
 import { requiredChain } from '@/connectors'
@@ -37,8 +39,25 @@ const CollateralTokenSelector = () => {
   return <Selector OptionEl={TokenDetails} name="collateralToken" options={tokens} />
 }
 
+const NoBondFound = () => {
+  const { data: account } = useAccount()
+  return (
+    <div className="p-4 space-y-4 w-full text-xs text-white rounded-md form-control">
+      <div className="flex justify-between w-full">
+        <PRTRIcon />
+        <div className="flex flex-col">
+          <span>No Bonds available to auction.</span> {!account?.address && 'Connect wallet'}
+        </div>
+      </div>
+    </div>
+  )
+}
+
 export const BondSelector = () => {
-  const { data } = useBondsPortfolio()
+  const { data, loading } = useBondsPortfolio()
+  if (!data?.length && !loading) {
+    return <Selector OptionEl={NoBondFound} name="bondToAuction" options={data} />
+  }
   return <Selector OptionEl={BondTokenDetails} name="bondToAuction" options={data} />
 }
 
