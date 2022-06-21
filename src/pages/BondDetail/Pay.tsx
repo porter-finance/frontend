1bfa13efe740adde3d612597d77b8a15fdd09a5c
Matt <Geczy@users.noreply.github.com>
2022-06-21 11:55:25 -0500

    fix pay manage

--- src/pages/BondDetail/Pay.tsx
+++ src/pages/BondDetail/Pay.tsx
@@ -2,7 +2,7 @@ import React, { useState } from 'react'
 
 import { formatUnits, parseUnits } from '@ethersproject/units'
 import dayjs from 'dayjs'
-import { useContractWrite } from 'wagmi'
+import { useContractRead, useContractWrite } from 'wagmi'
 
 import { SummaryItem } from '@/components/ProductCreate/SummaryItem'
 import { ActionButton } from '@/components/auction/Claimer'
@@ -10,7 +10,6 @@ import AmountInputPanel from '@/components/form/AmountInputPanel'
 import WarningModal from '@/components/modals/WarningModal'
 import BOND_ABI from '@/constants/abis/bond.json'
 import { Bond } from '@/generated/graphql'
-import { getValuePerBond } from '@/hooks/useBondExtraDetails'
 
 export const Pay = ({
   bond,
@@ -25,10 +24,11 @@ export const Pay = ({
     | 'amountUnpaid'
     | 'collateralToken'
     | 'collateralRatio'
+    | 'collateralTokenAmount'
+    | 'convertibleTokenAmount'
   >
 }) => {
   const [bondAmount, setBondAmount] = useState('0')
-  const collateralPerBond = bond ? getValuePerBond(bond, bond?.collateralRatio) : 0
   const { error, isError, isLoading, reset, write } = useContractWrite(
     {
       addressOrName: bond?.id,
@@ -37,17 +37,25 @@ export const Pay = ({
     'pay',
   )
 
-  const onMax = () => {
-    setBondAmount(formatUnits(bond?.amountUnpaid, bond?.paymentToken?.decimals))
-  }
+  const amountOwed = formatUnits(bond?.amountUnpaid, bond?.paymentToken?.decimals)
 
+  const onMax = () => {
+    setBondAmount(amountOwed)
+  }
+  const { data: previewWithdrawExcessCollateralAfterPayment } = useContractRead(
+    { addressOrName: bond?.id, contractInterface: BOND_ABI },
+    'previewWithdrawExcessCollateralAfterPayment',
+    { args: parseUnits(bondAmount, bond?.paymentToken?.decimals) },
+  )
+  const result = (previewWithdrawExcessCollateralAfterPayment || '0').toString()
+  const excessCollateralDisplay = Number(
+    formatUnits(result, bond?.collateralToken?.decimals),
+  ).toLocaleString()
   return (
     <div className="space-y-2">
       <SummaryItem
         border={false}
-        text={`${Number(
-          formatUnits(bond?.amountUnpaid, bond?.paymentToken?.decimals),
-        ).toLocaleString()} USDC`}
+        text={`${Number(amountOwed).toLocaleString()} USDC`}
         tip="Amount owed"
         title="Amount owed"
       />
@@ -67,13 +75,11 @@ export const Pay = ({
         onMax={onMax}
         onUserSellAmountInput={setBondAmount}
         token={bond?.paymentToken}
-        value={bondAmount}
+        value={bondAmount || ''}
       />
       <SummaryItem
         border={false}
-        text={`${(collateralPerBond * Number(bondAmount)).toLocaleString()} ${
-          bond.collateralToken.symbol
-        }`}
+        text={`${excessCollateralDisplay} ${bond.collateralToken.symbol}`}
         tip="Amount of collateral unlocked"
         title="Amount of collateral unlocked"
       />
