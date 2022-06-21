84b80edd420eeb5818c81f30325979886dbd266d
Matt <Geczy@users.noreply.github.com>
2022-06-21 13:11:55 -0500

    two withdraw methods

--- src/pages/BondDetail/Withdraw.tsx
+++ src/pages/BondDetail/Withdraw.tsx
@@ -1,7 +1,7 @@
 import React, { useState } from 'react'
 
 import { formatUnits, parseUnits } from '@ethersproject/units'
-import { useContractWrite } from 'wagmi'
+import { useContractRead, useContractWrite } from 'wagmi'
 
 import { SummaryItem } from '@/components/ProductCreate/SummaryItem'
 import { ActionButton } from '@/components/auction/Claimer'
@@ -22,9 +22,12 @@ export const Withdraw = ({
     | 'collateralToken'
     | 'convertibleTokenAmount'
     | 'collateralRatio'
+    | 'maxSupply'
+    | 'amountUnpaid'
   >
 }) => {
-  const [bondAmount, setBondAmount] = useState('0')
+  const [collateralAmount, setCollateralAmount] = useState('0')
+  const [paymentAmount, setPaymentAmount] = useState('0')
   const { error, isError, isLoading, reset, write } = useContractWrite(
     {
       addressOrName: bond?.id,
@@ -32,49 +35,115 @@ export const Withdraw = ({
     },
     'withdrawExcessCollateral',
   )
-  const onMax = () => {
-    setBondAmount(formatUnits(bond?.convertibleTokenAmount, bond?.collateralToken?.decimals))
+  const onMaxCollateral = () => {
+    setCollateralAmount(
+      formatUnits(previewWithdrawExcessCollateral.toString(), bond?.collateralToken?.decimals),
+    )
   }
 
-  return (
-    <div className="space-y-2">
-      <SummaryItem
-        border={false}
-        text={`${Number(
-          formatUnits(bond?.collateralTokenAmount, bond?.collateralToken?.decimals),
-        ).toLocaleString()} ${bond?.collateralToken?.symbol}`}
-        tip="Collateral locked"
-        title="Collateral locked"
-      />
-      <SummaryItem
-        border={false}
-        text={`${Number(
-          formatUnits(bond?.convertibleTokenAmount, bond?.collateralToken?.decimals),
-        ).toLocaleString()} ${bond?.collateralToken?.symbol}`}
-        tip="Collateral available to withdraw"
-        title="Collateral available to withdraw"
-      />
-      <AmountInputPanel
-        amountText="Amount of collateral to withdraw"
-        amountTooltip="This is your withdraw amount"
-        maxTitle="Withdraw all"
-        onMax={onMax}
-        onUserSellAmountInput={setBondAmount}
-        token={bond?.collateralToken}
-        value={bondAmount || ''}
-      />
+  const onMaxPayment = () => {
+    setPaymentAmount(
+      formatUnits(previewWithdrawExcessPayment.toString(), bond?.paymentToken?.decimals),
+    )
+  }
 
-      <ActionButton
-        className={`${isLoading ? 'loading' : ''}`}
-        onClick={() =>
-          write({
-            args: [parseUnits(bondAmount, bond?.collateralToken.decimals), bond?.owner],
-          })
-        }
-      >
-        Withdraw
-      </ActionButton>
-      <WarningModal content={error?.message} isOpen={isError} onDismiss={reset} />
-    </div>
+  const { data: previewWithdrawExcessCollateral } = useContractRead(
+    { addressOrName: bond?.id, contractInterface: BOND_ABI },
+    'previewWithdrawExcessCollateral',
+  )
+  const excessCollateralDisplay = Number(
+    formatUnits(
+      (previewWithdrawExcessCollateral || '0').toString(),
+      bond?.collateralToken?.decimals,
+    ),
+  ).toLocaleString()
+
+  const { data: previewWithdrawExcessPayment } = useContractRead(
+    { addressOrName: bond?.id, contractInterface: BOND_ABI },
+    'previewWithdrawExcessPayment',
+  )
+  const excessPaymentDisplay = Number(
+    formatUnits((previewWithdrawExcessPayment || '0').toString(), bond?.paymentToken?.decimals),
+  ).toLocaleString()
+
+  return (
+    <>
+      <div className="space-y-2">
+        <SummaryItem
+          border={false}
+          text={`${Number(
+            formatUnits(bond?.collateralTokenAmount, bond?.collateralToken?.decimals),
+          ).toLocaleString()} ${bond?.collateralToken?.symbol}`}
+          tip="The amount of total collateral in the Bond contract."
+          title="Collateral locked"
+        />
+        <SummaryItem
+          border={false}
+          text={`${excessCollateralDisplay} ${bond?.collateralToken?.symbol}`}
+          tip="At the moment, the amount of collateral available to withdraw from the contract."
+          title="Collateral available to withdraw"
+        />
+        <AmountInputPanel
+          amountText="Amount of collateral to withdraw"
+          amountTooltip="This is your withdraw amount"
+          maxTitle="Withdraw all"
+          onMax={onMaxCollateral}
+          onUserSellAmountInput={setCollateralAmount}
+          token={bond?.collateralToken}
+          value={collateralAmount || ''}
+        />
+
+        <ActionButton
+          className={`${isLoading ? 'loading' : ''}`}
+          onClick={() =>
+            write({
+              args: [parseUnits(collateralAmount, bond?.collateralToken.decimals), bond?.owner],
+            })
+          }
+        >
+          Withdraw Collateral
+        </ActionButton>
+      </div>
+      <div className="space-y-2">
+        <SummaryItem
+          border={false}
+          text={`${Number(
+            formatUnits(
+              Math.abs(bond?.maxSupply - bond?.amountUnpaid).toString(),
+              bond?.paymentToken?.decimals,
+            ),
+          ).toLocaleString()} ${bond?.paymentToken?.symbol}`}
+          tip="The amount of total payment in the Bond contract."
+          title="Payment locked"
+        />
+        <SummaryItem
+          border={false}
+          text={`${excessPaymentDisplay} ${bond?.paymentToken?.symbol}`}
+          tip="At the moment, the amount of payment available to withdraw from the contract."
+          title="Payment available to withdraw"
+        />
+        <AmountInputPanel
+          amountText="Amount of payment to withdraw"
+          amountTooltip="This is your withdraw amount"
+          maxTitle="Withdraw all"
+          onMax={onMaxPayment}
+          onUserSellAmountInput={setPaymentAmount}
+          token={bond?.paymentToken}
+          value={paymentAmount || ''}
+        />
+
+        <ActionButton
+          className={`${isLoading ? 'loading' : ''}`}
+          onClick={() =>
+            write({
+              args: [parseUnits(paymentAmount, bond?.paymentToken.decimals), bond?.owner],
+            })
+          }
+        >
+          Withdraw Payment
+        </ActionButton>
+        <WarningModal content={error?.message} isOpen={isError} onDismiss={reset} />
+      </div>
+    </>
   )
 }
